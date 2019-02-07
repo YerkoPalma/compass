f76041832a67c204e88196dad9e9b5ff1dd79b04
Yerko Palma <yerko.palma@usach.cl>
2019-02-07 16:23:38 -0300

    add web-compass component

---
+++ components/web-compass.js
@@ -0,0 +1,190 @@
+/* global customElements */
+import { LitElement, html } from 'https://unpkg.com/@polymer/lit-element?module'
+import { getBrowserOrientation, popup, decimalToSexagesimal } from '../lib/utils.js'
+
+export default class WebCompass extends LitElement {
+  static get properties () {
+    return {
+      debug: {
+        type: Boolean
+      },
+      hng: {
+        type: Number
+      },
+      phase: {
+        type: Number
+      },
+      lat: {
+        type: Number
+      },
+      lng: {
+        type: Number
+      }
+    }
+  }
+
+  firstUpdated () {
+    if (window.screen.width > window.screen.height) {
+      this.defaultOrientation = 'landscape'
+    } else {
+      this.defaultOrientation = 'portrait'
+    }
+    navigator.geolocation.watchPosition(({ coords }) => {
+      this.lat = coords.latitude
+      this.lng = coords.longitude
+    }, err => console.error(err), {
+      enableHighAccuracy: false,
+      maximumAge: 30000,
+      timeout: 27000
+    })
+
+    window.addEventListener('deviceorientation', event => {
+      var heading = event.alpha
+
+      if (typeof event.webkitCompassHeading !== 'undefined') {
+        heading = event.webkitCompassHeading // iOS non-standard
+      }
+
+      this.orientation = getBrowserOrientation()
+
+      if (typeof heading !== 'undefined' && heading !== null) { // && typeof orientation !== 'undefined') {
+        // we have a browser that reports device heading and orientation
+
+        // what adjustment we have to add to rotation to allow for current device orientation
+        var adjustment = 0
+        if (this.defaultOrientation === 'landscape') {
+          adjustment -= 90
+        }
+
+        if (typeof orientation !== 'undefined') {
+          var currentOrientation = this.orientation.split('-')
+
+          if (this.defaultOrientation !== currentOrientation[0]) {
+            if (this.defaultOrientation === 'landscape') {
+              adjustment -= 270
+            } else {
+              adjustment -= 90
+            }
+          }
+
+          if (currentOrientation[1] === 'secondary') {
+            adjustment -= 180
+          }
+        }
+
+        this.hng = heading + adjustment
+
+        this.phase = this.hng < 0 ? 360 + this.hng : this.hng
+      } else {
+        // device can't show heading
+        this.showHeadingWarning()
+      }
+    })
+  }
+  showHeadingWarning () {
+    if (!this.warningHeadingShown) {
+      popup(`<p>
+      Unfortunately this browser doesn't support orientation so will not show your correct heading.
+    </p>`)
+      this.warningHeadingShown = true
+    }
+  }
+  render () {
+    return html`
+    <style>
+    :host [class^="column-"] {
+      float: left;
+    }
+    
+      :host .column-33 {
+        width: 33.33333%;
+      }
+    
+      :host .column-25 {
+        width: 25%;
+      }
+    
+      :host .label {
+        font-size: 0.5rem;
+      }
+    :host .row {
+      padding-top: 1.5rem;
+    }
+    :host .position {
+      padding-bottom: 0.5rem;
+      text-transform: uppercase;
+      text-align: center;
+    }
+    :host .compass {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+      }
+      
+      :host .compass__rose {
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+        }
+      
+        :host .compass__rose__dial {
+            height: 100%;
+            width: 100%;
+          }
+      
+          :host .compass__pointer {
+          height: 100%;
+          width: 100%;
+        }
+    </style>
+  <div class="compass">
+    <div style="transform: rotateZ(${this.hng}deg); -webkit-transform: rotateZ(${this.hng}deg)" id="rose" class="compass__rose">
+      <svg class="compass__rose__dial" viewBox="0 0 130 130" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="65" cy="65" r="56" stroke="white" stroke-width="1" fill="none" />
+        <polyline points="63,9  67,9  65,13" fill="white" />
+        <polyline points="121,63  121,67  119,65" fill="white" />
+        <polyline points="63,121  67,121  65,119" fill="white" />
+        <polyline points="9,63  9,67  11,65" fill="white" />
+
+        <text x="65" y="4.2" font-size="5" text-anchor="middle" fill="white">N</text>
+        <text x="127" y="67" font-size="5" text-anchor="middle" fill="white">E</text>
+        <text x="65" y="129" font-size="5" text-anchor="middle" fill="white">S</text>
+        <text x="2.8" y="67" font-size="5" text-anchor="middle" fill="white">W</text>
+
+      </svg>
+
+    </div>
+
+    <svg class="compass__pointer" viewBox="0 0 130 130" version="1.1" xmlns="http://www.w3.org/2000/svg">
+
+      <polyline points="60,60  70,60  65,15" fill="#b60000" />
+      <polyline points="60,70  70,70  65,115" fill="white" />
+      <circle cx="65" cy="65" r="7" stroke="#b60000" stroke-width="7" fill="none" />
+
+    </svg>
+
+  </div>
+  ${this.debug ? html`<div id='debug-orientation-default'>${this.orientation}</div>
+  <div id='debug-orientation'>${this.defaultOrientation}</div>` : ''}
+
+  <div class='position row'>
+    <div class='column-33'>
+      <div class='label'>HDG</div>
+      <div id='position-hng'>${this.phase ? (360 - this.phase | 0) + '°' : 'n/a'}</div>
+    </div>
+    <div class='column-33'>
+      <div class='label'>Lat</div>
+      <div id='position-lat'>${this.lat ? decimalToSexagesimal(this.lat, 'lat') : 'n/a'}</div>
+    </div>
+    <div class='column-33'>
+      <div class='label'>Lng</div>
+      <div id='position-lng'>${this.lng ? decimalToSexagesimal(this.lng, 'lng') : 'n/a'}</div>
+    </div>
+  </div>`
+  }
+}
+
+customElements.define('web-compass', WebCompass)
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 /* global screen */
-import { popup } from './lib/utils.js'
+import { popup, getBrowserOrientation } from './lib/utils.js'
+import './components/web-compass.js'
 
 (function () {
   'use strict'
-
-  // set to true for debugging output
-  var debug = false
 
   // our current position
   var positionCurrent = {
@@ -13,18 +11,6 @@ import { popup } from './lib/utils.js'
     lng: null,
     hng: null
   }
-
-  // the outer part of the compass that rotates
-  var rose = document.getElementById('rose')
-
-  // elements that ouput our position
-  var positionLat = document.getElementById('position-lat')
-  var positionLng = document.getElementById('position-lng')
-  var positionHng = document.getElementById('position-hng')
-
-  // debug outputs
-  var debugOrientation = document.getElementById('debug-orientation')
-  var debugOrientationDefault = document.getElementById('debug-orientation-default')
 
   // info popup elements, pus buttons that open popups
   var btnsPopup = document.querySelectorAll('.btn-popup')
@@ -35,49 +21,10 @@ import { popup } from './lib/utils.js'
   var btnMap = document.getElementById('btn-map')
   var btnInfo = document.getElementById('btn-info')
 
-  // if we have shown the heading unavailable warning yet
-  var warningHeadingShown = false
-
   // switches keeping track of our current app state
   var isOrientationLockable = false
   var isOrientationLocked = false
   var isNightMode = false
-
-  // the orientation of the device on app load
-  var defaultOrientation
-
-  // browser agnostic orientation
-  function getBrowserOrientation () {
-    var orientation
-    if (screen.orientation && screen.orientation.type) {
-      orientation = screen.orientation.type
-    } else {
-      orientation = screen.orientation ||
-                    screen.mozOrientation ||
-                    screen.msOrientation
-    }
-
-    /*
-      'portait-primary':      for (screen width < screen height, e.g. phone, phablet, small tablet)
-                                device is in 'normal' orientation
-                              for (screen width > screen height, e.g. large tablet, laptop)
-                                device has been turned 90deg clockwise from normal
-      'portait-secondary':    for (screen width < screen height)
-                                device has been turned 180deg from normal
-                              for (screen width > screen height)
-                                device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
-      'landscape-primary':    for (screen width < screen height)
-                                device has been turned 90deg clockwise from normal
-                              for (screen width > screen height)
-                                device is in 'normal' orientation
-      'landscape-secondary':  for (screen width < screen height)
-                                device has been turned 90deg anti-clockwise (or 270deg clockwise) from normal
-                              for (screen width > screen height)
-                                device has been turned 180deg from normal
-    */
-
-    return orientation
-  }
 
   // browser agnostic orientation unlock
   function browserUnlockOrientation () {
@@ -128,73 +75,6 @@ import { popup } from './lib/utils.js'
       document.mozCancelFullScreen()
     } else if (document.msExitFullscreen) {
       document.msExitFullscreen()
-    }
-  }
-
-  // called on device orientation change
-  function onHeadingChange (event) {
-    var heading = event.alpha
-
-    if (typeof event.webkitCompassHeading !== 'undefined') {
-      heading = event.webkitCompassHeading // iOS non-standard
-    }
-
-    var orientation = getBrowserOrientation()
-
-    if (typeof heading !== 'undefined' && heading !== null) { // && typeof orientation !== 'undefined') {
-      // we have a browser that reports device heading and orientation
-
-      if (debug) {
-        debugOrientation.textContent = orientation
-      }
-
-      // what adjustment we have to add to rotation to allow for current device orientation
-      var adjustment = 0
-      if (defaultOrientation === 'landscape') {
-        adjustment -= 90
-      }
-
-      if (typeof orientation !== 'undefined') {
-        var currentOrientation = orientation.split('-')
-
-        if (defaultOrientation !== currentOrientation[0]) {
-          if (defaultOrientation === 'landscape') {
-            adjustment -= 270
-          } else {
-            adjustment -= 90
-          }
-        }
-
-        if (currentOrientation[1] === 'secondary') {
-          adjustment -= 180
-        }
-      }
-
-      positionCurrent.hng = heading + adjustment
-
-      var phase = positionCurrent.hng < 0 ? 360 + positionCurrent.hng : positionCurrent.hng
-      positionHng.textContent = (360 - phase | 0) + '°'
-
-      // apply rotation to compass rose
-      if (typeof rose.style.transform !== 'undefined') {
-        rose.style.transform = 'rotateZ(' + positionCurrent.hng + 'deg)'
-      } else if (typeof rose.style.webkitTransform !== 'undefined') {
-        rose.style.webkitTransform = 'rotateZ(' + positionCurrent.hng + 'deg)'
-      }
-    } else {
-      // device can't show heading
-
-      positionHng.textContent = 'n/a'
-      showHeadingWarning()
-    }
-  }
-
-  function showHeadingWarning () {
-    if (!warningHeadingShown) {
-      popup(`<p>
-      Unfortunately this browser doesn't support orientation so will not show your correct heading.
-    </p>`)
-      warningHeadingShown = true
     }
   }
 
@@ -281,20 +161,6 @@ import { popup } from './lib/utils.js'
     }
   }
 
-  function locationUpdate (position) {
-    positionCurrent.lat = position.coords.latitude
-    positionCurrent.lng = position.coords.longitude
-
-    positionLat.textContent = decimalToSexagesimal(positionCurrent.lat, 'lat')
-    positionLng.textContent = decimalToSexagesimal(positionCurrent.lng, 'lng')
-  }
-
-  function locationUpdateFail (error) {
-    positionLat.textContent = 'n/a'
-    positionLng.textContent = 'n/a'
-    console.log('location fail: ', error)
-  }
-
   function setNightmode (on) {
     if (on) {
       btnNightmode.classList.add('active')
@@ -321,38 +187,6 @@ import { popup } from './lib/utils.js'
     window.open('https://www.google.com/maps/place/@' + positionCurrent.lat + ',' + positionCurrent.lng + ',16z', '_blank')
   }
 
-  function decimalToSexagesimal (decimal, type) {
-    var degrees = decimal | 0
-    var fraction = Math.abs(decimal - degrees)
-    var minutes = (fraction * 60) | 0
-    var seconds = (fraction * 3600 - minutes * 60) | 0
-
-    var direction = ''
-    var positive = degrees > 0
-    degrees = Math.abs(degrees)
-    switch (type) {
-      case 'lat':
-        direction = positive ? 'N' : 'S'
-        break
-      case 'lng':
-        direction = positive ? 'E' : 'W'
-        break
-    }
-
-    return degrees + '° ' + minutes + '\' ' + seconds + '" ' + direction
-  }
-
-  if (screen.width > screen.height) {
-    defaultOrientation = 'landscape'
-  } else {
-    defaultOrientation = 'portrait'
-  }
-  if (debug) {
-    debugOrientationDefault.textContent = defaultOrientation
-  }
-
-  window.addEventListener('deviceorientation', onHeadingChange)
-
   document.addEventListener('fullscreenchange', onFullscreenChange)
   document.addEventListener('webkitfullscreenchange', onFullscreenChange)
   document.addEventListener('mozfullscreenchange', onFullscreenChange)
@@ -371,12 +205,6 @@ import { popup } from './lib/utils.js'
     For more information, bugs or comments please visit <a href='https://github.com/lamplightdev/compass'>the repo on github</a>.
     </p>`))
   }
-
-  navigator.geolocation.watchPosition(locationUpdate, locationUpdateFail, {
-    enableHighAccuracy: false,
-    maximumAge: 30000,
-    timeout: 27000
-  })
 
   setNightmode(false)
   checkLockable()
