567b0ec4d3a32ebf648f7a376cc15477c2439d7e
Yerko Palma <yerko.palma@usach.cl>
2019-02-20 13:42:47 -0300

    use tinyify

--- build.js
+++ build.js
@@ -4,5 +4,6 @@ const babelify = require('babelify')
 
 browserify('./index.js')
   .transform(babelify.configure({ presets: [ '@babel/preset-env' ], plugins: [ '@babel/transform-runtime' ] }), { global: true })
+  .plugin('tinyify', { flat: false })
   .bundle()
   .pipe(fs.createWriteStream('bundle.js'))
